83664f13867b8cfbb917e01a51ce1e47bc8dfb18
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-07-27 10:52:30 +0530

    Ques

--- Lab02/Swap2nosRef.cpp
+++ Lab02/Swap2nosRef.cpp
@@ -14,7 +14,9 @@ int main()
     int a, b;
     cout << "Enter 2 nos:";
     cin >> a >> b;
+    cout << "a:" << a << " b:" << b << endl;
     fact(a, b);
-    cout << a << " " << b << endl;
+    cout << "After Swap" << endl;
+    cout << "a:" << a << " b:" << b << endl;
     return 0;
 }
--- Lab02/Swap2nosVal.cpp
+++ Lab02/Swap2nosVal.cpp
@@ -8,13 +8,15 @@ void fact(int a, int b)
     temp = a;
     a = b;
     b = temp;
-    cout << a << " " << b << endl;
+    cout << "After Swap" << endl;
+    cout << "a:" << a << " b:" << b << endl;
 }
 int main()
 {
     int a, b;
     cout << "Enter 2 nos:";
     cin >> a >> b;
+    cout << "a:" << a << " b:" << b << endl;
     fact(a, b);
     return 0;
 }
