73cbb2384549fdb06750a6db03bad29bd222f017
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-10-26 10:19:45 +0530

    Ques

--- Lab08/Q07_matrixsum.cpp
+++ Lab08/Q07_matrixsum.cpp
@@ -17,15 +17,17 @@ public:
             }
         }
     }
-    void operator-()
+    Matrix operator+(Matrix m1)
     {
+        Matrix t;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                a[i][j] = -a[i][j];
+                t.a[i][j] = a[i][j] + m1.a[i][j];
             }
         }
+        return t;
     }
     void display()
     {
@@ -43,11 +45,15 @@ public:
 
 int main()
 {
-    cout << "\nBefore negative operator:\n";
+    cout << "\nMatrix 1:\n";
     Matrix m1;
     m1.input();
     m1.display();
-    cout << "\nAfter negative operator:\n";
-    -m1;
-    m1.display();
+    cout << "\nMatrix 2:\n";
+    Matrix m2;
+    m2.input();
+    m2.display();
+    cout << "\nThe sum of two matrix is:\n";
+    Matrix m3 = m1 + m2;
+    m3.display();
 }
