69a80fe50f4a3e4847fa5d8bcb335c1f192f9292
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-11-22 19:24:36 +0530

    Ques

---
+++ Lab11/Q02_MultipleTemp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+template <typename T, typename U>
+void display(T a, U b)
+{
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+}
+
+int main()
+{
+    int a = 10;
+    float b = 10.5;
+    display(a, b);
+    return 0;
+}
---
+++ Lab11/Q03_MultipleTemp.cpp
@@ -0,0 +1,54 @@
+// WAP to find sort an integer array and a float array, using class template..
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Sort
+{
+    T a[5];
+
+public:
+    void getdata()
+    {
+        cout << "Enter 5 elements: ";
+        for (int i = 0; i < 5; i++)
+            cin >> a[i];
+    }
+    void sort()
+    {
+        T temp;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = i + 1; j < 5; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+    void display()
+    {
+        cout << "Sorted array is: ";
+        for (int i = 0; i < 5; i++)
+            cout << a[i] << " ";
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Sort<int> s1;
+    Sort<float> s2;
+    s1.getdata();
+    s1.sort();
+    s1.display();
+    s2.getdata();
+    s2.sort();
+    s2.display();
+    return 0;
+}
