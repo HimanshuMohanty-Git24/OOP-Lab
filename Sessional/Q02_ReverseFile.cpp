9658e927174f2441dd05a6ec3b52649378cb2a07
Himanshu Mohanty <codehimanshu24@gmail.com>
2022-11-23 09:52:44 +0530

    QUES SESSIONAL

--- Sessional/Q02_ReverseFile.cpp
+++ Sessional/Q02_ReverseFile.cpp
@@ -1,4 +1,22 @@
 /*2.WAP to display content of a file in reverse order. */
-#include <iostream>
 #include <fstream>
+#include <iostream>
+
 using namespace std;
+
+int main()
+{
+
+    ifstream in("File1.txt");
+
+    in.seekg(0, in.end);
+    int len = in.tellg();
+
+    char c;
+    while (len--)
+    {
+        in.seekg(len, in.beg);
+        in >> c;
+        cout << c;
+    }
+}
